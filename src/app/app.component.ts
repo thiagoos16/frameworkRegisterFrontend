65a2ac4cd49b0812f73505337ff57075bbe57b00
thiagometal <thiagometal.fne@gmail.com>
2018-07-19 23:36:28 -0400

    add nfor to list frameworks fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  frameworks = [
+        {"id": 1,
+        "name": "Flask",
+        "url_image": "url",
+        "site": "www.flask.com.br",
+        "year_creation": 2012,
+        "creator": "Leonard Stuart",
+        "latest_stable_release": "4",
+        "type": "Back",
+        "opinion": "very good",
+        "pros_cons": "Nothing to Say",
+        "id_language": 4,
+        "created_at": "2018-07-19 22:00:35",
+        "updated_at": "2018-07-19 22:00:35",
+        "language": "Python"}
+  ];  
 }
