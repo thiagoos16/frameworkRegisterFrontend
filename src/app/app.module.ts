b51ce9753e054de8273393b95f8de976e93142a9
thiagometal <thiagometal.fne@gmail.com>
2018-07-20 01:55:09 -0400

    add forms module and adjust form framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FrameworkService } from './framework.service';
@@ -11,7 +12,8 @@ import { FrameworkService } from './framework.service';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [FrameworkService],
   bootstrap: [AppComponent]
