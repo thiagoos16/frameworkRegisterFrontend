785c007c97638b8a90c2a63fd63755bb742f8799
thiagometal <thiagometal.fne@gmail.com>
2018-07-19 23:55:58 -0400

    add list frameworks route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -9,7 +10,8 @@ import { FrameworkService } from './framework.service';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [FrameworkService],
   bootstrap: [AppComponent]
--- src/app/framework.service.ts
+++ src/app/framework.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FrameworkService {
 
-  constructor() { }
+  frameworkUrl = 'http://localhost:8000/framework';
+
+  constructor(private http: HttpClient) { }
+
+  getFrameworks() {
+      return this.http.get<any[]>(`${this.frameworkUrl}`);
+  }
 }
